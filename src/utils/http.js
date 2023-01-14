f2ae0fc7208cafe7e4e2ff836ca2dfb4fe753f3c
aiyaya <15958116062@163.com>
2023-01-14 20:40:43 +0800

    请求添加token

--- src/utils/http.js
+++ src/utils/http.js
@@ -1,4 +1,18 @@
 import axios from 'axios';
-import Vue from 'vue'
+import Vue from 'vue';
+
+// 设置axios拦截器
+axios.interceptors.request.use(function (config) {
+    // 在发送请求之前做些什么
+    if (config.url !== '/login') {
+        // 请求带上token 身份认证
+        const token = sessionStorage.getItem('token');
+        config.headers.Authorization = token;
+    }
+    return config;
+  }, function (error) {
+    // 对请求错误做些什么
+    return Promise.reject(error);
+  });
 
 Vue.prototype.$axios = axios;
